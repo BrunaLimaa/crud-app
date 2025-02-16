43f18d2a29e9494a62dbb8395f4aeadba0b97491
[Bruna Rosa] <brunarblima93@gmail.com>
2025-02-15 23:07:29 -0300

    first commit

---
+++ Connect.php
@@ -0,0 +1,19 @@
+<?php
+  
+    define("HOSTNAME", "localhost");
+    define("USERNAME", "root");
+    define("PASSWORD", "");
+    define("DATABASE", "students");
+
+    $connection = mysqli_connect(HOSTNAME, USERNAME, PASSWORD, DATABASE);
+
+    if(!$connection){
+        echo "Error: ".mysqli_connect_error();
+        exit();
+    }
+    else{
+        echo "Connected";
+    }
+
+
+?>
