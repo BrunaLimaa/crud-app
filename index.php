50edf47a935f533b5441931f20c82d1a1bcacf6c
[Bruna Rosa] <brunarblima93@gmail.com>
2025-02-16 23:56:07 -0300

    add home page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html long="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP Test</title>
+        </head>
+        <body>
+            <h1>HomePage</h1>
+            <?php echo '<p>Hello World</p>'; ?>
+        </body>
+<html>
